04274685c51cedfcdeb6ea64bfa520ed891bdae7
D4RKH0R1Z0N <76612351+D4RKH0R1Z0N@users.noreply.github.com>
2021-12-14 14:52:05 +0530

    Create macchanger.py

---
+++ macchanger.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+
+import os
+
+def change_mac(interface, mac):
+  os.system("ifconfig " + interface + " down")
+  os.system("ifconfig " + interface + " hw " + "ether " + mac)
+  os.system("ifconfig " + interface + " up")
+
+interface = input("Enter which Interface you want to change the MAC : ")
+new_mac = input("Enter you new MAC address : ")
+
+change_mac(interface, mac)
